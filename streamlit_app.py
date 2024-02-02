d979a485051bc29fdd652a8ba4e3e83a581c6def
R.Girinath <125426416+girinath18@users.noreply.github.com>
2024-02-02 10:56:13 +0530

    Update streamlit_app.py
    add error handling to the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,30 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import cv2
-import os
 
 # Load your trained model
-model = load_model(r'C:\Users\Dell 5470\Desktop\Aadhaar Detection Using Streamlit\model\Aasteroid.h5')
+model_path = r'C:\Users\Dell 5470\OneDrive\Desktop\Aadhaar Detection Using Streamlit\model\Ashes.h5'
+model = load_model(model_path)
 
 def predict_aadhaar(image_path):
-    # Load and preprocess the image
-    img = image.load_img(image_path, target_size=(128, 128))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize the image
+    try:
+        # Load and preprocess the image
+        img = image.load_img(image_path, target_size=(128, 128))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0  # Normalize the image
 
-    # Make predictions
-    prediction = model.predict(img_array)
+        # Make predictions
+        prediction = model.predict(img_array)
 
-    # Assuming your model has two classes (0: Not Aadhaar, 1: Aadhaar)
-    predicted_class = np.argmax(prediction)
+        # Assuming your model has two classes (0: Not Aadhaar, 1: Aadhaar)
+        predicted_class = np.argmax(prediction)
 
-    # Return True if predicted as Aadhaar, False otherwise
-    return predicted_class == 1
+        # Return True if predicted as Aadhaar, False otherwise
+        return predicted_class == 1
+    except Exception as e:
+        st.error(f"Error predicting the image: {e}")
+        return False
 
 def main():
     st.title("Aadhaar Detection App")
